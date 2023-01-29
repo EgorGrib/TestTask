cae319937f462ed4daecba79e6aa8189d935a474
unknown <egor111orexov@gmail.com>
2023-01-29 21:07:21 +0100

    second task

---
+++ Task2.sql
@@ -0,0 +1,7 @@
+﻿-- Запрос отношения many to many с помощью вспомогательной таблицы product_category
+SELECT PRODUCT.NAME, CATEGORY.NAME
+FROM PRODUCT LEFT JOIN product_category
+ON product_category.PRODUCT_ID=PRODUCT.PRODUCT_ID
+LEFT JOIN CATEGORY
+ON product_category.CATEGORY_ID=CATEGORY.CATEGORY_ID
+
